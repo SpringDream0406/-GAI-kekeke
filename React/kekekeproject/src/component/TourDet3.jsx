7923f7505436bd6c50b37b6a61d1a2bdfe0ab2a0
KimEunHo02 <eunhog2016@gmail.com>
2023-11-09 14:02:59 +0900

    [feat]TourDet3페이지디자인
    저번에올렸었는데 오늘봐보니 이상해져서 다시 디자인 구성했습니다

--- React/kekekeproject/src/component/TourDet3.jsx
+++ React/kekekeproject/src/component/TourDet3.jsx
@@ -31,14 +31,15 @@ export const TourDet3 = () => {
 
 
                   <div className="tourdet3-SampleStoreReviewFr"> {/* 샘플케이크, 매장정보, 리뷰 탭 div */}
-                 
-                      <div className="text-TourDet3sampleTx">샘플케이크</div>
-                    
-                      <div className="text-TourDet3sampleTx">매장정보</div>
-                  
-                    
-                      <div className="tourdet3_ReviewTabTx">리뷰</div>
-                    
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">샘플케이크</div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">매장정보</div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="tour-det-review">리뷰</div>
+                    </div>
                   </div>{/* 샘플케이크, 매장정보, 리뷰 탭 div 끝*/} 
                 </div>
               </div>
@@ -48,24 +49,23 @@ export const TourDet3 = () => {
 
               <div className="storeinfo"> {/* 가게이미지,케이크집이름, 주소, 가게설명, 1:1:채팅div*/}
                 <div className="tourdet">
-                  <img className="tourdet3-StoreLogo"
+                  <img className="tourdet3-StoreLog"
                     src={tourdet3_StoreLogo}
                     alt={CakeName}
                      />{/*가게 로고*/}
                 </div>
+                <div className="adfsadf">
                 
-                <div className="tourdet-3">
                   <div className="tourdet3-CakeStoreNameFrame">
                     <div className="tourdet3-CakeStoreName">{CakeStoreName}</div>{/* 가게 이름 넣는곳*/}
-                  </div>
-                 
                     <div className="tour-det-addr-tx">{StoreAddr}</div>{/* 가게 주소넣는곳*/}
-                 
+                  </div>
                   <div className="tour-det-det">
                     <p className="tour-det-det-tx" >
                     {StoreDetail}{/* 가게 설명넣는곳*/}
                     </p>
                   </div>
+                
                 </div>
                 
                   <div className="overlap-group-2">
