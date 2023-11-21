62ec7da7600f048c2ffc46d14582837430d8fad8
yoekjang2 <yoekjang2@gmail.com>
2023-11-21 13:15:33 +0900

    [fix] 커스터머 회원가입 오류 수정

--- React/kekekeproject/config/imgStorage.js
+++ React/kekekeproject/config/imgStorage.js
@@ -1,4 +1,7 @@
 const multer = require('multer');
+const path = require('path');
+const { join_check } = require('./join');
+
 
 const allowedImageExtensions = ['.jpg', '.jpeg', '.png']; // 이미지 허용 확장자
 
@@ -8,20 +11,45 @@ const imgStorage = (imgPath, imgName) => multer.diskStorage({
         cb(null, imgPath);
     },
     filename: (req, file, cb) => {
+        // body의 해당값으로 이름 지정
+        const fileName = req.body[imgName];
+
         // 확장자 가져오기
         const extname = path.extname(file.originalname);
 
-        // 허용된 확장자인지 체크
-        if (!allowedImageExtensions.includes(extname.toLowerCase())) { // 확장자 소문자로 변환 후 허용 확장자 리스트에 있는지 확인
-            return cb(new Error('허용되지 않는 이미지 확장자입니다.'));
-        }
-
-        // 확장자로 이름 설정
-        const filename = `${imgName}${extname}`;
+        // 이름 설정
+        const filename = `${fileName}${extname}`;
         cb(null, filename);
     },
+
 });
 
-const imgUpload = multer({ storage: imgStorage });
 
-module.exports = { imgStorage, imgUpload };
+const cust_fileFilter = (req, file, cb) => {
+
+        // 이미지가 있을 때 제한사항 통과하기 전에 이미지가 저장되면 안되므로
+        // 제한사항 체크를 여기서도 해줘야함
+
+        // 확장자 가져오기
+        const extname = path.extname(file.originalname).toLowerCase();
+
+        let { cust_id, nick_name, cust_pw, cust_pwcheck, phone } = req.body;
+        let nullCheck = !(cust_id && nick_name && cust_pw && cust_pwcheck && phone);
+        let join_check_result = join_check(cust_id, cust_pw, cust_pwcheck)
+
+        if (!allowedImageExtensions.includes(extname)) {
+            req.fileValidationError = "jpg, jpeg, png 파일만 업로드 가능합니다.";
+            cb(null, false);
+        } else if (nullCheck) {
+            req.fileValidationError = "빈칸이 존재 합니다.";
+            cb(null, false);
+        } else if (join_check_result) {
+            req.fileValidationError = join_check_result;
+            cb(null, false);
+        }
+        else {
+            cb(null, true);
+        }
+}
+
+module.exports = { imgStorage, cust_fileFilter };
