ebc6065aa22114d17e59cb9931c3629540c8de17
yujeongbbang <pzzzz12@naver.com>
2023-11-22 17:45:41 +0900

    [fix] 어드민 코드 수정1

--- React/kekekeproject/src/ad_pages/AdMain.js
+++ React/kekekeproject/src/ad_pages/AdMain.js
@@ -1,4 +1,4 @@
-import React , { useState }from 'react'
+import React, { useState } from 'react'
 import AdMenubar from '../component/AdMenubar'
 import AdBG from '../ad_component/AdBG'
 import AdMT from '../ad_component/AdMT'
@@ -10,156 +10,159 @@ import AdHeader from '../component/AdHeader'
 
 
 const AdMain = () => {
-  
-  const [activeSection, setActiveSection] = useState('ongoing'); // 'ongoing' 또는 'completed'
- 
- // 임시 주문 데이터 상태
- const [orders] = useState([
-  { id: 1, cakeName: 'ㅇㅇ케이크', userName: '정건식', orderDate: '2023.03.10' },
-  { id: 2, cakeName: 'ㅇㅇ케이크', userName: '서유정', orderDate: '2023.03.10' },
-  { id: 3, cakeName: 'ㅇㅇ케이크', userName: '송민아', orderDate: '2023.03.10' },
-  { id: 4, cakeName: 'ㅇㅇ케이크', userName: '김은호', orderDate: '2023.03.10' },
-  { id: 5, cakeName: 'ㅇㅇ케이크', userName: '김용민', orderDate: '2023.03.10' },
-]);
- // 주문 목록 렌더링 함수
- const renderOrderList = () => {
-  return orders.map((order) => (
-    <Link key={order.id} className='admain-list-all' to={`/order/${order.id}`}> {/* 임시 */}
 
-      <div className='adlist-cname'>{order.cakeName}</div>
-      <div className='adlist-uname'>{order.userName}</div>
-      <div className='adlist-listday'>{order.orderDate}</div>
-      <hr className='adlist-hr'/>
-    </Link>
-  ));
-};
+  const [activeSection, setActiveSection] = useState('ongoing'); // 'ongoing' 또는 'completed'
+
+  // 임시 주문 데이터 상태
+  const [orders] = useState([
+    { id: 1, cakeName: 'ㅇㅇ케이크', userName: '정건식', orderDate: '2023.03.10' },
+    { id: 2, cakeName: 'ㅇㅇ케이크', userName: '서유정', orderDate: '2023.03.10' },
+    { id: 3, cakeName: 'ㅇㅇ케이크', userName: '송민아', orderDate: '2023.03.10' },
+    { id: 4, cakeName: 'ㅇㅇ케이크', userName: '김은호', orderDate: '2023.03.10' },
+    { id: 5, cakeName: 'ㅇㅇ케이크', userName: '김용민', orderDate: '2023.03.10' },
+  ]);
+  // 주문 목록 렌더링 함수
+  const renderOrderList = () => {
+    return orders.map((order) => (
+      <Link key={order.id} className='admain-list-all' to={`/order/${order.id}`}> {/* 임시 */}
+
+        <div className='adlist-cname'>{order.cakeName}</div>
+        <div className='adlist-uname'>{order.userName}</div>
+        <div className='adlist-listday'>{order.orderDate}</div>
+        <hr className='adlist-hr' />
+      </Link>
+    ));
+  };
 
 
   return (
     <div>
-     
-<AdHeader></AdHeader>
-        <AdMenubar/>
-        
-        <AdMT>주문현황</AdMT>
 
-        <AdBG height={1430}>
-          
+      <AdHeader></AdHeader>
+      <AdMenubar />
 
-    <div className='ad-main-continer'>
+      <AdMT>주문현황</AdMT>
 
-         <div className={`admain-bfmsg ${activeSection === 'ongoing' ? 'active' : ''}`} onClick={() => setActiveSection('ongoing')}>상담중 10</div>
-         <div className={`admain-afmsg ${activeSection === 'completed' ? 'active' : ''}`} onClick={() => setActiveSection('completed')}>주문완료 10</div>
-
-{/* activeSection 상태에 따라 조건부 렌더링 */}
-{activeSection === 'ongoing' && (
-        <div className='admain-msgbg-all'>
-          <Link className='admain-msgbg1' to='/admin/message' >
-            <div className='admain-msgmt'>채팅</div>
-            <div className='admain-msgst'>상담중</div>
-            <div className='admain-msg-cir'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg' alt='msg-uimg'/>
-            <div className='admin-msg-uname'>정건식 고객님</div>
-            <div className='admin-msg-uday'>2023.10.30 접수</div>
-          </Link>
-
-          <Link className='admain-msgbg1' to='/admin/message' >
-            <div className='admain-msgmt'>채팅</div>
-            <div className='admain-msgst'>상담중</div>
-            <div className='admain-msg-cir'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg'  alt='msg-uimg'/>
-            <div className='admin-msg-uname'>정건식 고객님</div>
-            <div className='admin-msg-uday'>2023.10.30 접수</div>
-          </Link>
-
-          <Link className='admain-msgbg1' to='/admin/message' >
-            <div className='admain-msgmt'>채팅</div>
-            <div className='admain-msgst'>상담중</div>
-            <div className='admain-msg-cir'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg'  alt='msg-uimg'/>
-            <div className='admin-msg-uname'>정건식 고객님</div>
-            <div className='admin-msg-uday'>2023.10.30 접수</div>
-          </Link>
-
-          <Link className='admain-msgbg1' to='/admin/message' >
-            <div className='admain-msgmt'>채팅</div>
-            <div className='admain-msgst'>상담중</div>
-            <div className='admain-msg-cir'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg'  alt='msg-uimg'/>
-            <div className='admin-msg-uname'>정건식 고객님</div>
-            <div className='admin-msg-uday'>2023.10.30 접수</div>
-          </Link>
-          
-        </div>
-)}
-         
-
-         {activeSection === 'completed' && (
-
-         <div className='admain-msgbg-all2'>
-
-          <Link className='admain-msgbg2' to='/admin/message' >
-            <div className='admain-msgmt2'>채팅</div>
-            <div className='admain-msgst2'>상담완료</div>
-            <div className='admain-msg-cir2'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2'  alt='msg-uimg'/>
-            <div className='admin-msg-uname2'>정건식 고객님</div>
-            <div className='admin-msg-uday2'>2023.10.30 접수</div>
-          </Link>
-
-          <Link className='admain-msgbg2' to='/admin/message' >
-            <div className='admain-msgmt2'>채팅</div>
-            <div className='admain-msgst2'>상담완료</div>
-            <div className='admain-msg-cir2'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2'  alt='msg-uimg'/>
-            <div className='admin-msg-uname2'>정건식 고객님</div>
-            <div className='admin-msg-uday2'>2023.10.30 접수</div>
-          </Link>
+      <AdBG height={1430}>
 
 
-          <Link className='admain-msgbg2' to='/admin/message' >
-            <div className='admain-msgmt2'>채팅</div>
-            <div className='admain-msgst2'>상담완료</div>
-            <div className='admain-msg-cir2'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2'  alt='msg-uimg'/>
-            <div className='admin-msg-uname2'>정건식 고객님</div>
-            <div className='admin-msg-uday2'>2023.10.30 접수</div>
-          </Link>
+        <div className='ad-main-continer'>
+          {/* activeSection 상태에 따라 조건부 렌더링 */}
 
-          <Link className='admain-msgbg2' to='/admin/message' >
-            <div className='admain-msgmt2'>채팅</div>
-            <div className='admain-msgst2'>상담완료</div>
-            <div className='admain-msg-cir2'/>
-            <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2'  alt='msg-uimg'/>
-            <div className='admin-msg-uname2'>정건식 고객님</div>
-            <div className='admin-msg-uday2'>2023.10.30 접수</div>
-          </Link>
+          <div className={`admain-bfmsg ${activeSection === 'ongoing' ? 'active' : ''}`} onClick={() => setActiveSection('ongoing')}>상담중 10</div>
+          <div className={`admain-afmsg ${activeSection === 'completed' ? 'active' : ''}`} onClick={() => setActiveSection('completed')}>주문완료 10</div>
 
-        </div>
-         )}
+          <Link className='admsgPlus' to='/admin/message'>+ 더보기</Link>
+
+          {activeSection === 'ongoing' && (
+            <div className='admain-msgbg-all'>
+              <Link className='admain-msgbg1' to='/admin/message' >
+                <div className='admain-msgmt'>채팅</div>
+                <div className='admain-msgst'>상담중</div>
+                <div className='admain-msg-cir' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg' alt='msg-uimg' />
+                <div className='admin-msg-uname'>정건식 고객님</div>
+                <div className='admin-msg-uday'>2023.10.30 접수</div>
+              </Link>
+
+              <Link className='admain-msgbg1' to='/admin/message' >
+                <div className='admain-msgmt'>채팅</div>
+                <div className='admain-msgst'>상담중</div>
+                <div className='admain-msg-cir' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg' alt='msg-uimg' />
+                <div className='admin-msg-uname'>정건식 고객님</div>
+                <div className='admin-msg-uday'>2023.10.30 접수</div>
+              </Link>
+
+              <Link className='admain-msgbg1' to='/admin/message' >
+                <div className='admain-msgmt'>채팅</div>
+                <div className='admain-msgst'>상담중</div>
+                <div className='admain-msg-cir' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg' alt='msg-uimg' />
+                <div className='admin-msg-uname'>정건식 고객님</div>
+                <div className='admin-msg-uday'>2023.10.30 접수</div>
+              </Link>
+
+              <Link className='admain-msgbg1' to='/admin/message' >
+                <div className='admain-msgmt'>채팅</div>
+                <div className='admain-msgst'>상담중</div>
+                <div className='admain-msg-cir' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg' alt='msg-uimg' />
+                <div className='admin-msg-uname'>정건식 고객님</div>
+                <div className='admin-msg-uday'>2023.10.30 접수</div>
+              </Link>
+
+            </div>
+          )}
+
+
+          {activeSection === 'completed' && (
+
+            <div className='admain-msgbg-all2'>
+
+              <Link className='admain-msgbg2' to='/admin/message' >
+                <div className='admain-msgmt2'>채팅</div>
+                <div className='admain-msgst2'>상담완료</div>
+                <div className='admain-msg-cir2' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2' alt='msg-uimg' />
+                <div className='admin-msg-uname2'>정건식 고객님</div>
+                <div className='admin-msg-uday2'>2023.10.30 접수</div>
+              </Link>
+
+              <Link className='admain-msgbg2' to='/admin/message' >
+                <div className='admain-msgmt2'>채팅</div>
+                <div className='admain-msgst2'>상담완료</div>
+                <div className='admain-msg-cir2' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2' alt='msg-uimg' />
+                <div className='admin-msg-uname2'>정건식 고객님</div>
+                <div className='admin-msg-uday2'>2023.10.30 접수</div>
+              </Link>
+
+
+              <Link className='admain-msgbg2' to='/admin/message' >
+                <div className='admain-msgmt2'>채팅</div>
+                <div className='admain-msgst2'>상담완료</div>
+                <div className='admain-msg-cir2' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2' alt='msg-uimg' />
+                <div className='admin-msg-uname2'>정건식 고객님</div>
+                <div className='admin-msg-uday2'>2023.10.30 접수</div>
+              </Link>
+
+              <Link className='admain-msgbg2' to='/admin/message' >
+                <div className='admain-msgmt2'>채팅</div>
+                <div className='admain-msgst2'>상담완료</div>
+                <div className='admain-msg-cir2' />
+                <img src={'assets/images/cake1.jpg'} className='admin-msg-uimg2' alt='msg-uimg' />
+                <div className='admin-msg-uname2'>정건식 고객님</div>
+                <div className='admin-msg-uday2'>2023.10.30 접수</div>
+              </Link>
+
+            </div>
+          )}
 
 
 
 
 
-         <div className='admain-mt2'>주문내역</div>
-         <div className='admain-listtitle-all'>
+          <div className='admain-mt2'>주문내역</div>
+          <Link className='adproductPlus' to='/admin/orderlist'>+ 더보기</Link>
+          <div className='admain-listtitle-all'>
             <div className='admain-list-mcn'>케이크 이름</div>
             <div className='admain-list-muname'>닉네임</div>
             <div className='admain-list-mday'>예약날짜</div>
-         </div>
-         <div className='admain-list-constainer'>
-         {renderOrderList()}
-         </div>
+          </div>
+          <div className='admain-list-constainer'>
+            {renderOrderList()}
+          </div>
 
 
-         
-         <div className='admain-mt3'>판매량순위</div>
-       <AdBarChart />
 
-       </div>
-        </AdBG>
-        
+          <div className='admain-mt3'>판매량순위</div>
+          <AdBarChart />
+
+        </div>
+      </AdBG>
+
 
     </div>
   )
--- React/kekekeproject/src/ad_pages/AdminOrderlist.jsx
+++ React/kekekeproject/src/ad_pages/AdminOrderlist.jsx
@@ -18,7 +18,6 @@ const AdminOrderlist = () => {
             cakeFlavor: '다크 초콜릿',
             cakeText: '생일 축하해!',
             orderDate: '2023-11-22',
-            pickupStatus: '픽업 대기',
             price: '35000원',
             buyerName: '정건식',
             buyerPhone: '010-1234-5678',
@@ -32,7 +31,6 @@ const AdminOrderlist = () => {
             cakeFlavor: '바닐라',
             cakeText: '케케케 수고해또',
             orderDate: '2023-11-18',
-            pickupStatus: '픽업 완료',
             price: '46000원',
             buyerName: '서유정',
             buyerPhone: '010-1234-5678',
@@ -46,7 +44,6 @@ const AdminOrderlist = () => {
             cakeFlavor: '오레오',
             cakeText: '로또 번호',
             orderDate: '2023-11-22',
-            pickupStatus: '픽업 대기',
             price: '35000원',
             buyerName: '정건식',
             buyerPhone: '010-1234-5678',
@@ -60,7 +57,6 @@ const AdminOrderlist = () => {
             cakeFlavor: '다크 초콜릿',
             cakeText: '생일 축하해!',
             orderDate: '2023-11-22',
-            pickupStatus: '픽업 대기',
             price: '35000원',
             buyerName: '정건식',
             buyerPhone: '010-1234-5678',
@@ -122,7 +118,6 @@ const onPageChange = (pageNumber) => {
                             </div>
                             <div className="AOOrderInfo">
                                 <p>{order.orderDate}</p>
-                                <p>{order.pickupStatus}</p>
                                 <p>가격: {order.price}</p>
                             </div>
                             <div className="AOBuyer">
